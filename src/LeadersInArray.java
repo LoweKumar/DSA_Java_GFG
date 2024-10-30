2b4fc4e7dd82c827008a771730325be3fa811c97
Lowe Kumar <lowekr00@gmail.com>
2024-10-30 13:16:25 +0530

    find leader in array using arrayList

--- src/LeadersInArray.java
+++ src/LeadersInArray.java
@@ -45,3 +45,39 @@ class LeadersInArray
         printLeaderss(arr, n);
 	}
 }
+
+// using arrayList 
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+// public class LeadersInArray {
+
+//     public static void main(String[] args) {
+//         int[] arr = {16, 17, 4, 3, 5, 2}; // Sample array
+//         ArrayList<Integer> leaders = findLeaders(arr);
+        
+//         System.out.println("Leaders in the array: " + leaders);
+//     }
+
+//     public static ArrayList<Integer> findLeaders(int[] arr) {
+//         ArrayList<Integer> leaders = new ArrayList<>();
+//         int n = arr.length;
+
+//         // Start from the last element
+//         int maxFromRight = arr[n - 1];
+//         leaders.add(maxFromRight); // The last element is always a leader
+
+//         // Traverse the array from second last to the first element
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (arr[i] > maxFromRight) {
+//                 maxFromRight = arr[i]; // Update maxFromRight
+//                 leaders.add(arr[i]); // Add leader to the list
+//             }
+//         }
+
+//         // Reverse the leaders list to maintain the order of appearance
+//         Collections.reverse(leaders);
+//         return leaders;
+//     }
+// }
+
